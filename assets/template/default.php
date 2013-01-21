f6a2431d273e6d7e78e77be48f4eb85f134d8bc7
Marc Harding <info@marcharding.de>
2013-01-21 22:21:23 +0100

    Updated to HTML5.

--- assets/template/default.php
+++ assets/template/default.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="utf-8"/>
 	<title><?php echo ( $q->g( 'title' ) ) ? $q->g( 'title' ) . ' » ' : '' ?>Example</title>
 	<meta name="description" content="<?php echo ( $q->g( 'description' ) ) ?>" />
 	<meta name="content-language" content="en" />
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="shortcut icon" href="/favicon.ico" />
 	<link rel="stylesheet" href="/assets/css/default.css" type="text/css" media="screen" />
 	<!--[if lte IE 6]><link rel="stylesheet" href="/assets/css/ie6.css" type="text/css" media="screen" /><![endif]-->
 	<?php if ( $q->g( 'javascript' ) ) { ?>
-		<script src="/assets/js/javascript.js" type="text/javascript" charset="utf-8"></script>
+		<script src="/assets/js/javascript.js"></script>
 	<?php } ?>
 </head>
 <body>
